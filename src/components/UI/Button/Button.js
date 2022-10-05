10e0f70e94dafe561d817c19878f01f6a0108345
amitmahlawat <amitmahlawat3@gmail.com>
2022-10-05 20:41:20 +0530

    initial commit

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,28 @@
+import React,{useState} from 'react';
+
+import './Button.css';
+
+const Button = props => {
+// const[isValid,setIsValid]=useState(true)
+
+// const clickHandler=()=>{
+//   if(props.enteredValue.trim().length===0){
+//     setIsValid(false)
+//     return;
+//   }else{
+//     setIsValid(true)
+
+//   }
+// }
+console.log(props.isValid)
+
+
+
+  return (
+    <button style={{background: !props.isValid ? 'green' : 'ac0e77'}}  type={props.type} className="button">
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
